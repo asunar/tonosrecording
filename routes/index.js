c5e6b30d6212ccd54072102b1fc9695a8fdfa5e4
Alper Sunar <ASunar@paylocity.com>
2015-01-10 15:26:39 -0600

    Added charge post handler

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,9 @@ exports.index = function(req, res){
 		firstName: 'Test',
 		lastName: 'Tester',
 		email: 'test@test.com',
-		eventDate_Month: 1,
-		eventDate_Day: 1,
-		eventDate_Year: 2015,
-		eventDate_Hour: 6,
-		eventDate_Minute: 0,
-		eventDate_AMPM: [ {value: 'AM', text: 'AM'}, { value: 'PM', text: 'PM'  }],
-		eventDate_AMPM_selected:  {"value": 'PM'},
 		locations: [ { value: 'West Houston Indoor Soccer', text: 'West Houston Indoor Soccer'  } ],
-		selectedLocation: { value: 'west Houston Indoor Soccer'  }
-
+		selectedLocation: { value: 'west Houston Indoor Soccer'  },
+		messages: [{ type: "Warning", text: "A warning message" }]
 	};
 
   res.render('index', paymentInfo );
