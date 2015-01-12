77af6188f1c42798f5eadeeb458a0918d407cb82
Alper Sunar <ASunar@paylocity.com>
2015-01-12 17:39:01 -0600

    Removed hardcoded values, messages

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,11 @@
 exports.index = function(req, res){
 	var paymentInfo = {
 		title: 'Recording Payment',
-		firstName: 'Test',
-		lastName: 'Tester',
-		email: 'test@test.com',
+		firstName: '',
+		lastName: '',
+		email: '',
 		locations: [ { value: 'West Houston Indoor Soccer', text: 'West Houston Indoor Soccer'  } ],
 		selectedLocation: { value: 'west Houston Indoor Soccer'  },
-		messages: [{ type: "Warning", text: "A warning message" }]
 	};
 
   res.render('index', paymentInfo );
