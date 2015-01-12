7dcf8ecf687903c5f4f0add857a88b9e0d524971
Alper Sunar <ASunar@paylocity.com>
2015-01-12 17:32:35 -0600

    Added script to toggle pay button based on required fields

--- app.js
+++ app.js
@@ -32,7 +32,27 @@ app.get('/', routes.index);
 app.post('/charge', function (req, res) {
 	console.log('Posted');
 	console.log(req.body);	
+	chargeCard(req.body.stripeToken);
 });
+
+
+function chargeCard(stripeToken){
+	var stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+
+	var charge = stripe.charges.create({
+		amount: 3000, // amount in cents, again
+		currency: "usd",
+		card: stripeToken,
+		description: "payinguser@example.com"
+	}, function(err, charge) {
+		if (err && err.type === 'StripeCardError') {
+			// The card has been declined
+		}
+		else {
+			console.log(charge);
+		}
+	});	
+}
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
